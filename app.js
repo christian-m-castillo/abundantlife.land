425c8c1a6728b2ef0d1611bd8255071fef180166
christian-m-castillo <christian.castillo.email@gmail.com>
2025-01-17 13:15:31 -0600

    wireframe and copy 1.0

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-/*script*/
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.remove('active');
+  });
+});
